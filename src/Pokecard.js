b65683555d39feb2e62b1735517d2fef37dcb8f0
Helena Johansson <helena@klarr.se>
2020-12-16 12:12:15 +0100

    pokecard

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Pokecard extends Component {
   render() {
     return (
-      <div>
-        <h1>Pokedex!</h1>
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
         <div>Type: {this.props.type}</div>
         <div>EXP: {this.props.exp}</div>
       </div>
