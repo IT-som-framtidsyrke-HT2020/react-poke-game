91f4bfd53deb59c7cc09a7621f17a29be3a0b89b
Helena Johansson <helena@klarr.se>
2020-12-18 10:29:55 +0100

    new images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 import "./Pokecard.css";
 
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
 class Pokecard extends Component {
   render() {
-    let imageSrc = `${POKE_API}${this.props.id}.png`;
+    const threeDigit = `00${this.props.id}`.slice(-3);
+    let imageSrc = `${POKE_API}${threeDigit}.png`;
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
